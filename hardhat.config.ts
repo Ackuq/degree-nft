64e8c087dc3ba562de39195bfd67001e2c372f0b
Axel Pettersson <axel@pettersson.cc>
2022-08-14 20:49:56 +0200

    Make scripts more generic

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import dotenv from "dotenv";
+import { ethers } from "hardhat";
 dotenv.config();
 
 const { ALCHEMY_API_KEY, PRIVATE_KEY, NETWORK = "goerli" } = process.env;
 
 console.log(`Using network ${NETWORK}...`);
+
+const provider = new ethers.providers.AlchemyProvider(NETWORK, ALCHEMY_API_KEY);
 
 if (!ALCHEMY_API_KEY || !PRIVATE_KEY) {
   throw new Error(
@@ -15,10 +18,10 @@ if (!ALCHEMY_API_KEY || !PRIVATE_KEY) {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  defaultNetwork: NETWORK,
+  defaultNetwork: provider.network.name,
   networks: {
-    [NETWORK]: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    [provider.network.name]: {
+      ...provider.network,
       accounts: [PRIVATE_KEY],
     },
   },
--- scripts/mint-nft.ts
+++ scripts/mint-nft.ts
@@ -1,40 +1,77 @@
 import dotenv from "dotenv";
 import contract from "../artifacts/contracts/DegreeNFT.sol/DegreeNFT.json";
 import * as ethers from "ethers";
+import readline from "readline";
 
 // Get env vars
 dotenv.config();
-const { ALCHEMY_API_KEY, PRIVATE_KEY, NETWORK } = process.env;
+const {
+  ALCHEMY_API_KEY,
+  PRIVATE_KEY,
+  CONTRACT_ADDRESS,
+  NETWORK = "goerli",
+} = process.env;
 const provider = new ethers.providers.AlchemyProvider(NETWORK, ALCHEMY_API_KEY);
-
-const contractAddress = "0xC4422cB08e79b4be7B3BAf967C00248405C5490d";
 
 // Create a signer
 if (!PRIVATE_KEY) {
-  console.error("MetaMask `PRIVATE_KEY` not specified, exiting...");
+  console.error("`PRIVATE_KEY` not specified, exiting...");
+  process.exit(1);
+} else if (!ALCHEMY_API_KEY) {
+  console.error("`ALCHEMY_API_KEY` not specified, exiting...");
+  process.exit(1);
+} else if (!CONTRACT_ADDRESS) {
+  console.error("`CONTRACT_ADDRESS` not specified, exiting...");
   process.exit(1);
 }
 
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
 const abi = contract.abi;
-const nftContract = new ethers.Contract(contractAddress, abi, signer);
+const nftContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
 const mintNFT = async (tokenURI: string) => {
+  // Mint the new NFT
   let nftTxn = await nftContract.safeMint(signer.address, tokenURI);
   await nftTxn.wait();
-  console.log(
-    `NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`
-  );
+  const link =
+    (NETWORK === "mainnet"
+      ? "https://etherscan.io/tx/"
+      : `https://${NETWORK}.etherscan.io/tx/`) + nftTxn.hash;
+  console.log(`NFT Minted! Check it out at: ${link}`);
 };
 
-const requestedURI = process.argv[2];
-if (!requestedURI) {
-  console.error("First argument should be the URI of the NFT resource.");
-  process.exit(1);
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-mintNFT(requestedURI).catch((error) => {
-  console.error(error);
-  process.exit(1);
+rl.question("IPFS URI for metadata: ", (tokenURI: string) => {
+  // Make sure the token URI is valid
+  if (!tokenURI.startsWith("ipfs://")) {
+    console.error("Invalid IPFS URI specified, must start with `ipfs://`");
+    rl.close();
+    process.exit(1);
+  }
+  const httpsURI =
+    tokenURI.replace("ipfs://", "https://") + ".ipfs.ipfs-gateway.cloud";
+  // Layer of security to be able to double check the metadata
+  rl.question(
+    `Do you want to mint using metadata found at ${httpsURI}, yes or no? `,
+    (answer) => {
+      if (answer.toLowerCase() === "y" || answer.toLowerCase() === "yes") {
+        mintNFT(tokenURI).catch((error) => {
+          console.error(error);
+          process.exit(1);
+        });
+      } else {
+        console.log("Exiting program without minting...");
+      }
+      rl.close();
+    }
+  );
+});
+
+rl.on("close", () => {
+  process.exit(0);
 });
